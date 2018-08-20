f1b0ef05f290a6b64f6c734087c49c48dd0b3210
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-08-20 17:22:55 +0000

    automatically backed up by learn

--- db/migrate/20180820171641_create_dogs.rb
+++ db/migrate/20180820171641_create_dogs.rb
@@ -1,12 +1,14 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
-    create_table :dogs do |t|
-      t.string :name
-      t.string :breed
-    end
-  end
 
-  def down
-    drop_table :dogs
-  end
+  # below code blocks are specified and implicit, respectively, in the above change method
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  # 
+  # def down
+  #   drop_table :dogs
+  # end
 end
